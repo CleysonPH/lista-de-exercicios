cf1050a25d47983a770f97a2fec097bec6ca0db3
Cleyson Lima <cleysonph@gmail.com>
2018-07-28 17:49:44 -0300

    Exercicio 07 de Estrutura Sequencial

---
+++ estrutura_sequencial/exercicio_07.py
@@ -0,0 +1,8 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+side = float(input('Lado do Quadrado: '))
+
+area = side**2
+
+print(f'Area do Quadrado = {area}')
+print(f'Dobro da Area do Quadrado = {area * 2}')
