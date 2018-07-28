eb81a49ac452697a9bff9f30032515553a072d53
Cleyson Lima <cleysonph@gmail.com>
2018-07-28 17:23:19 -0300

    Exercicio 06 de Estrutura Sequencial

---
+++ estrutura_sequencial/exercicio_06.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+from math import pi
+
+radius = float(input('Raio do Circulo: '))
+
+area = 2 * pi * radius
+
+print(f'Area do Circulo: {area}')
