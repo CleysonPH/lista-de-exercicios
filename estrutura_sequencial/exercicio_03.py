29f1e0ad76395c6fe697405d64d4843b24342d96
Cleyson Lima <cleysonph@gmail.com>
2018-07-28 17:11:24 -0300

    Exercicio 03 de Estrutura Sequencial

---
+++ estrutura_sequencial/exercicio_03.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima a soma.
+
+number1 = int(input('Numero 1: '))
+number2 = int(input('Numero 2: '))
+
+sum = number1 + number2
+
+print(f'Soma = {sum}')
