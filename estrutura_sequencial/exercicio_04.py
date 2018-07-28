84339b00dc500d4080f86ff286ac83397d3c41f7
Cleyson Lima <cleysonph@gmail.com>
2018-07-28 17:15:19 -0300

    Exercicio 04 de Estrutura Sequencial

---
+++ estrutura_sequencial/exercicio_04.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+grade1 = float(input('Nota 1: '))
+grade2 = float(input('Nota 2: '))
+grade3 = float(input('Nota 3: '))
+grade4 = float(input('Nota 4: '))
+
+average = (grade1+grade2+grade3+grade4) / 4
+
+print(f'Média = {average}')
