37bbb1018221611b602febb67cd4bc4263c7137c
Cleyson Lima <cleysonph@gmail.com>
2018-07-28 18:05:15 -0300

    Exercicio 10 de Estrutura Sequencial

---
+++ estrutura_sequencial/exercicio_10.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit.
+# Fomula -> F = 1.8C + 32
+
+celsius = float(input('Temperatura em Celsius: '))
+
+farenheit = 1.8*celsius + 21
+
+print(f'{celsius} graus em Celcius é equivalente á {farenheit} graus em Farenheit')
